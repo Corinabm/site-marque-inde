03703aba88ed473cc597768ccabd1b668e0d5d8e
Corinabm <bellemarecorina@gmail.com>
2025-08-01 19:08:55 -0400

    config model product

--- database/migrations/2025_08_01_212709_create_products_table.php
+++ database/migrations/2025_08_01_212709_create_products_table.php
@@ -13,6 +13,12 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('website_id')->contrained()->onDelete('cascade');
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->decimal('prix', 8, 2);
+            $table->string('url_product');
+            $table->string('image')->nullable;
             $table->timestamps();
         });
     }
